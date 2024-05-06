40d94298577e310d68f80b66d629e47d10a0a744
Nardo <nardokong@gmail.com>
2024-05-06 12:35:59 +0800

    fix layout

--- src/main/java/com/example/demo/Repositories/DisplayNumRepository.java
+++ src/main/java/com/example/demo/Repositories/DisplayNumRepository.java
@@ -3,5 +3,5 @@ package com.example.demo.Repositories;
 import com.example.demo.Entities.DisplayNum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DisplayNumRepository extends JpaRepository<DisplayNum, Integer> {
+public interface DisplayNumRepository extends JpaRepository<DisplayNum, String> {
 }
